8648bcd9837f9d8ffe963166c4906c7c0cb409db
Zaiqiang Wu <wuzaiqiang@zju.edu.cn>
2025-02-17 21:31:14 +0900

    updated

--- turntable_test.py
+++ turntable_test.py
@@ -8,8 +8,8 @@ def main():
     MAX_TARGET_POS = 4095
     #turntable.set_pos_raw(4000)
     #turntable1.set_pos_raw(4095//2)
-    for i in range(MIN_TARGET_POS, MAX_TARGET_POS,200):
-        time.sleep(5)
+    for i in range(MIN_TARGET_POS, MAX_TARGET_POS,800):
+        time.sleep(3)
         turntable1.set_pos_raw(i)
 
 
